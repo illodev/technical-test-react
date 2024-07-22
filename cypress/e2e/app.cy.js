fce18a8720878db5d98158ead52baef4e69c219b
capiloky <jauregui.alvaro@nimioestudio.com>
2024-07-22 21:45:31 +0200

    feat: Add Cypress e2e test

---
+++ cypress/e2e/app.cy.js
@@ -0,0 +1,8 @@
+import { cy, describe, it } from "cypress";
+
+describe("Navigation", () => {
+    it("should navigate to the about page", () => {
+        // Start from the index page
+        cy.visit("http://localhost:3000/");
+    });
+});
