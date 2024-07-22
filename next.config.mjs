8b88f8ef66a5662a0039ef0e710150fc128729d7
capiloky <jauregui.alvaro@nimioestudio.com>
2024-07-22 21:48:03 +0200

    feat: Add remotePatterns to Next.js image configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        dangerouslyAllowSVG: true,
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "**",
+            },
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ],
+    },
+    output: "standalone",
+};
 
 export default nextConfig;
